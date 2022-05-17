1ab90bceaa912d634973c8321aec552922ac88da
Che <53223732+che-a@users.noreply.github.com>
2022-05-17 23:40:32 +0300

    Create osdetect.sh

---
+++ osdetect.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Определение типа ОС: MS-Windows/Linux/MacOS
+# вывод будет одним из таких: Linux MSWin macOS
+
+if type -t wevtutil &> /dev/null
+then
+    OS=MSWin
+elif type -t scutil &> /dev/null
+then
+    OS=macOS
+else
+    OS=Linux
+fi
+
+echo $OS
