a43099674ca7ee8f024372c9af3e0aef3edd2417
Che <53223732+che-a@users.noreply.github.com>
2022-05-30 22:56:44 +0300

    Create script-framework.sh

---
+++ script-framework.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+PROGNAME="$(basename "$0")"
+
+usage () {
+	echo ""
+	echo "$PROGNAME: usage: $PROGNAME [-f file | -i]"
+	echo ""
+	return
+}
+
+
+interactive=
+filename=
+
+while [[ -n "$1" ]]; do
+	case "$1" in
+		-f | --file)		shift
+							filename="$1"							
+							;;
+		-i | --interactive)	interactive=1
+							;;
+		-h | --help)		usage
+							exit
+							;;
+		*)					usage >&2
+							exit 1
+							;;
+	esac
+	shift
+done
