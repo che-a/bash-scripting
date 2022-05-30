a857b19bc992d6f285f2810add2ba9126355d05d
Che <53223732+che-a@users.noreply.github.com>
2022-05-31 00:38:14 +0300

    Update script-framework.sh

--- script-framework.sh
+++ script-framework.sh
@@ -16,16 +16,16 @@ filename=
 while [[ -n "$1" ]]; do
 	case "$1" in
 		-f | --file)		shift
-							filename="$1"							
-							;;
+					filename="$1"							
+					;;
 		-i | --interactive)	interactive=1
-							;;
+					;;
 		-h | --help)		usage
-							exit
-							;;
-		*)					usage >&2
-							exit 1
-							;;
+					exit
+					;;
+		*)			usage >&2
+					exit 1
+					;;
 	esac
 	shift
 done
