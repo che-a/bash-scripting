0e62e943f9e73bf47d5eb7fd0ac2258d5425837e
Che <53223732+che-a@users.noreply.github.com>
2023-09-01 17:17:46 +0300

    Update script-framework.sh

--- script-framework.sh
+++ script-framework.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
-PROGNAME=`basename "${0}"`
+prg_name=$(basename "${0}")
 
 usage () {
-	echo ""
-	echo "Usage: ""$PROGNAME"" [-f file | -i]"
-	echo ""
+	printf "\nUsage: %s [-f file | -i]\n\n" "${prg_name}"
 	return
 }
 
@@ -13,16 +11,19 @@ usage () {
 interactive=
 filename=
 
+# Перебор параметров
 while [[ -n "$1" ]]; do
 	case "$1" in
-		-f | --file)		shift
-					filename="$1"							
-					;;
+		-f | --file)        shift
+					        filename="$1"
+					        echo "${filename}"
+					        ;;
 		-i | --interactive)	interactive=1
-					;;
+		                    printf "flag interactive = %s\n" "${interactive}"
+					        ;;
 		-h | --help)		usage
-					exit
-					;;
+					        exit
+					        ;;
 		*)			usage >&2
 					exit 1
 					;;
